248eb397e787247be7c473a72dc3e4fb77cc3c09
António Nuno Monteiro <anmonteiro@gmail.com>
2014-05-16 02:05:51 +0100

    fix #2

--- test/mns.test.js
+++ test/mns.test.js
@@ -104,6 +104,71 @@ describe('an mns object', function() {
 
 });
 
+describe('HNScraper parse article element function', function () {
+  var el,
+    invalidEl,
+    obj,
+    $,
+    scraper;
+
+  beforeEach(function() {
+    
+  });
+
+  afterEach(function() {
+    el = null;
+    invalidEl = null;
+    obj = null;
+  });
+
+  it('should parse one DOM element with the article link into an object', function () {
+    el = [
+      '<td class="title"><a href="http://www.buildyourownlisp.com/">',
+      'Learn C and build your own programming language</a>',
+      '<span class="comhead"> (buildyourownlisp.com) </span></td>'
+    ].join( '' );
+    obj = {
+      title : 'Learn C and build your own programming language',
+      url: 'http://www.buildyourownlisp.com/',
+      src: ' (buildyourownlisp.com) ',
+    };
+    $ = cheerio.load(el);
+
+    scraper = mns( hn );
+    scraper.parseArticleElement($(el), function(err, art) {
+      expect(err).to.be.null;
+      expect(err).not.to.be.undefined;
+      art.should.be.ok;
+      art.should.be.an('object');
+      art.should.not.be.empty;
+      art.should.eql(obj);
+    });
+  });
+
+  it('should parse titles with special characters', function () {
+    el = [
+      '<td class="title"><a href="http://www.foo.tag/">',
+      'The &lt;foo&gt; tag</a>',
+      '<span class="comhead"> (foo.tag) </span></td>'
+    ].join( '' );
+    obj = {
+      title : 'The &lt;foo&gt; tag',
+      url: 'http://www.foo.tag/',
+      src: ' (foo.tag) ',
+    };
+    $ = cheerio.load(el);
+    scraper = mns( hn );
+    scraper.parseArticleElement($(el), function(err, art) {
+      expect(err).to.be.null;
+      expect(err).not.to.be.undefined;
+      art.should.be.ok;
+      art.should.be.an('object');
+      art.should.not.be.empty;
+      art.should.eql(obj);
+    });
+  });
+});
+
 describe('mns function execute', function() {
   var api;
 
